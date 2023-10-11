ed770eb181dec57de7c55d819b98fa9fe71ea6ae
sandeepsomavarapu <42529810+sandeepsomavarapu@users.noreply.github.com>
2023-10-11 12:57:43 +0530

    Welcome example using java

---
+++ Welcome.java
@@ -0,0 +1,15 @@
+class Welcome_123
+{
+public static void main(String args[])
+{
+	System.out.println("welcome to java");
+}
+}
+
+//compile  JDK -->JRE --->JVM  binary / byte code
+
+//compiler java source code --->byte code -->.class file
+
+//compailation Source code--->byte code -->input ->JVM
+
+//javac filename.java--->.class file-->java classname-->output
